79b8d5af40e5b2e12959735b8ea859a4100423ab
Eric <eric@combojs.com>
2018-01-30 15:39:11 -0600

    Added internal argument for future use.

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -23,14 +23,18 @@ export function _removeHTML(el) {
  * @private
  * @param {Element} el The element.
  * @param {string} html The HTML.
+ * @param {boolean} append Reserved for future use.
  *
  * @example
  *
  * _removeHTML(document.getElementById("root"));
  *
  */
-export function _replaceHTML(el, html) {
-	_removeHTML(el);
+export function _replaceHTML(el, html, append = false) {
+	if(append==false) {
+		_removeHTML(el);
+	}
+
 	el.insertAdjacentHTML("beforeEnd", html);
 }
 
